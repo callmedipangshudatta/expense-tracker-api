3a9ec9094a5d95081909faf02149c8011282665b
Dipangshu Datta <infoforddatta@gmail.com>
2026-08-09 13:39:19 +0530

    Building First real end point "GET /expense" of Spring Boot Expense Tracker API main() class

--- src/main/java/com/snehadipangshu/expense_tracker_api/ExpenseTrackerApiApplication.java
+++ src/main/java/com/snehadipangshu/expense_tracker_api/ExpenseTrackerApiApplication.java
@@ -1,47 +1,74 @@
+// We are using Spring Boot 4.1.0 VERSION
+
 package com.snehadipangshu.expense_tracker_api;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+// additionally including imports for hibernating our JPA request error
+
+import org.springframework.boot.jdbc.autoconfigure.DataSourceAutoConfiguration;
+import org.springframework.boot.hibernate.autoconfigure.HibernateJpaAutoConfiguration;
 
 // @SpringBootApplication is just like a teacher who approves a student assignment
 // Student is Java Class {ExpenseTrackerApiApplication}
 // Teacher is {@SpringBootApplication} which changes the meaning of the class
 
 // @SpringBootApplication is annotation : combines three different annotations
-// @Configuration + @EnableAutoConfiguration + @ComponentScan
+// @Configuration + @EnableAutoConfiguration + "@ComponentScan"
 // Responsibility : Start Application -> Configure Everything -> Search for Components
 
 // Spring sees this class and says
 //
 //"Aha! This is the main Spring Boot application."
 
-@SpringBootApplication
+@SpringBootApplication(
+
+//		@SpringBootApplication
+//        ↓
+//		@EnableAutoConfiguration
+//        ↓
+//		"Configure what you need,
+//		BUT exclude DataSource + Hibernate JPA."
+//		↓
+//		Web infrastructure can start
+//        ↓
+//		Tomcat
+//        ↓
+//		Controller
+
+		exclude = {
+				DataSourceAutoConfiguration.class,
+				HibernateJpaAutoConfiguration.class
+		}
+)
 public class ExpenseTrackerApiApplication {
 
 	public static void main(String[] args) {
 
-//				main()
-//		↓
-//				SpringApplication.run()
-//		↓
-//				SpringBoot Starts
-//		↓
-//				Read Dependencies
-//		↓
-//				Initialize Spring
-//		↓
-//				Initialize JPA
-//		↓
-//				JPA searches for "Where is Database?"
-//		↓
-//				prepares Database infrastructure
-// 		↓
-//				Starts Web-Server Tomcat
-//		↓
-//				Application Keep Running / Port 8080
-// 		↓
-//				Wait for HTTP Requests
+	// main()
+	//     ↓
+	// SpringApplication.run()
+	//     ↓
+	// Spring Boot starts
+	//     ↓
+	// Creates Spring Application Context
+	//     ↓
+	// Reads application configuration
+	//     ↓
+	// Finds Spring components
+	//     ↓
+	// Finds ExpenseController
+	//     ↓
+	// Starts embedded Tomcat
+	//     ↓
+	// Port 8080
+	//     ↓
+	// Waits for HTTP requests
 
+	// Temporary :-
+	// Database/JPA auto-configuration is currently excluded.
+	// We will configure MySQL + JPA properly later.
 
 
 
---
+++ src/main/java/com/snehadipangshu/expense_tracker_api/controller/ExpenseController.java
@@ -0,0 +1,48 @@
+package com.snehadipangshu.expense_tracker_api.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+//  Spring Boot starts
+//       ↓
+//  @SpringBootApplication
+//       ↓
+//  Spring scans application
+//       ↓
+//  Finds ExpenseController
+//       ↓
+//  Sees @RestController
+//       ↓
+//  "Okay, this class handles HTTP requests."
+
+//  @RestController is an annotation
+//  : extra information we attach to a class/method
+//  so that a framework can understand how to treat it
+
+//  basically tells Spring:
+//  "Spring, treat this class as a REST controller.
+//  It can receive HTTP requests and send HTTP responses."
+
+//  Level 1. Identify the Controller
+//  Mentioning @RestController tells to Spring this Java class
+//  can handle HTTP Requests and send HTTP Response
+//  but without any request until we put something like
+//  @GetMapping or @PostMapping
+@RestController
+
+public class ExpenseController{
+
+    //  tells Spring:
+    //  "For a GET request to /expenses, use this method."
+    //  Method : @GetMapping("/expenses")
+
+    //  Level 2. Give specific requests
+    //  When a GET request comes to /expenses,
+    //  Spring execute this method
+    @GetMapping("/expenses")
+    public String getExpenses(){
+        return "Here are the expenses";
+    }
+}
+
