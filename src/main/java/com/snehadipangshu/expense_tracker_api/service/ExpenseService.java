375735c567077bba6ab9b01b7167a85082042da1
Dipangshu Datta <infoforddatta@gmail.com>
2026-09-24 12:46:22 +0530

    updated : ExpenseService.java with comments containing information about each loc.

--- src/main/java/com/snehadipangshu/expense_tracker_api/service/ExpenseService.java
+++ src/main/java/com/snehadipangshu/expense_tracker_api/service/ExpenseService.java
@@ -1,18 +1,33 @@
+//  <--- Business Logic Layer --->
+
 package com.snehadipangshu.expense_tracker_api.service;
+//  importing and using the @Service annotation tells Spring Boot,
+//  "This is the control center where all our business math, validations, and core logic will happen."
 
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+//  These allow us to fetch-data in bite-sized chunks
 
 import com.snehadipangshu.expense_tracker_api.entity.Expense;
 import com.snehadipangshu.expense_tracker_api.repository.ExpenseRepository;
+//  The Service layer needs to manipulate data, so it brings our Expense vault and
+//  the ExpenseRepository(our robotic librarian) to issue commands like "repository.save()"
+
 import org.springframework.stereotype.Service;
 
 import java.awt.*;
 import java.util.List;
 import java.util.Optional;
+//  Optional is Java's way of forcing us to be safe.
+//       ↓
+//  Example : When you call repository.findById(99),
+//  Spring Boot no longer hands you the Expense directly.
+//  It hands you an Optional<Expense> (the sealed box).
 
-// @Service tells Spring: "This is a business logic class. Manage it for us"
 @Service
+//  @Service tells SpringBoot : "This is more like the brain of the operation.
+//  It handles the core business rules"
+
 public class ExpenseService {
 
     //We bring in the Repository so the Service can talk to the database
